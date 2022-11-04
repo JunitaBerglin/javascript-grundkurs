48fb8a01c1e98580285209c1d9af8b3ad49aa9ed
Junita Berglin <junita.berglin@gmail.com>
2022-11-04 15:17:20 +0100

    better version

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
 import { Todos } from "./todos";
 
-const mainTodoUl = document.getElementById("todoUl");
-
 let toDos = [];
 toDos.push(new Todos("Handla matvaror", false));
 toDos.push(new Todos("Karva Pumpor", false));
@@ -9,24 +7,43 @@ toDos.push(new Todos("Byta Sängkläder", false));
 toDos.push(new Todos("Dammsuga", true));
 
 function printTodoList() {
+  const mainTodoUl = document.getElementById("todoUl");
   mainTodoUl.innerHTML = "";
+
+  const clickedTodoUl = document.getElementById("clickedTodoUl");
+  clickedTodoUl.innerHTML = "";
 
   for (let i = 0; i < toDos.length; i++) {
     console.log(toDos[i]);
 
-    let newTodoLi = document.createElement("li");
-    newTodoLi.innerHTML = toDos[i].what;
-
-    newTodoLi.addEventListener("click", () => {
-      toDos[i].completed = true;
-      printTodoList();
-    });
-
     if (toDos[i].completed) {
-      newTodoLi.classList.add("beenClickedOn");
+      addToCompletedUl(toDos[i], clickedTodoUl);
+    } else {
+      stayAttodoUl(toDos[i], mainTodoUl);
     }
-
-    mainTodoUl.appendChild(newTodoLi);
   }
 }
+function addToCompletedUl(todos, clickedTodoUl) {
+  let newTodoLi = document.createElement("li");
+
+  newTodoLi.classList.add("beenClickedOn");
+  clickedTodoUl.appendChild(newTodoLi);
+  newTodoLi.innerHTML = todos.what;
+  newTodoLi.addEventListener("click", () => {
+    todos.completed = false;
+    todos.toggleComplete();
+    printTodoList();
+  });
+}
+
+function stayAttodoUl(todos, mainTodoUl) {
+  let newTodoLi = document.createElement("li");
+  mainTodoUl.appendChild(newTodoLi);
+  newTodoLi.innerHTML = todos.what;
+  newTodoLi.addEventListener("click", () => {
+    todos.completed = true;
+    printTodoList();
+  });
+}
+
 printTodoList();
